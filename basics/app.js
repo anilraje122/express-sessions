9e5029de659f721f09f805163cb9dc9e1d4f5dac
ip11 <iprash011@gmail.com>
2020-10-13 08:18:47 +0530

    Application Level Middleware

--- basics/app.js
+++ basics/app.js
@@ -2,6 +2,20 @@ const express = require("express");
 const app = express();
 
 const port = process.env.PORT || 3000;
+
+//Application Level Middleware
+app.use((req,res,next)=>{
+    console.log(req.query);
+    console.log("I am the Primary Hit");
+    next();
+});
+
+app.use((req,res,next)=>{
+    console.log(req.query);
+    console.log("I am the Secondary Hit");
+    next();
+});
+
 /*
 clientData :
 1)Query Params
@@ -36,7 +50,7 @@ app.get('/home', (req, res, next) => {
     console.log("I am Hit 1");
     next();
 
-}, (req, res,next) => {
+}, (req, res, next) => {
     console.log("I am Hit 2");
     console.log(req.some_value);
     res.send("Check Console for Query Data");
@@ -45,6 +59,34 @@ app.get('/home', (req, res, next) => {
     console.log("I am Hit 3");
     console.log(" I am End");
 });
+
+
+const cb0 = (req, res, next) => {
+    //Query Params
+    console.log(req.query);
+    req.some_value = "Prashanth";
+    console.log("I am Hit 1");
+    next();
+};
+const cb1 = (req, res, next) => {
+    console.log("I am Hit 2");
+    console.log(req.some_value);
+    res.send("Check Console for Query Data");
+    next();
+};
+
+const cb2 = (req, res) => {
+    console.log("I am Hit 3");
+    console.log(" I am End");
+};
+
+
+//next() example 2
+app.get('/root', cb0, cb1, cb2);
+
+
+//next() example 3
+app.get('/com', [cb0,cb1,cb2]);
 
 
 app.listen(port, () => {
