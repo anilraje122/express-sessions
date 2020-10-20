b275aa77d61e62590aea3a74b597b84bc4e13f51
ip11 <iprash011@gmail.com>
2020-10-20 06:42:36 +0530

    Addedd User Routes

--- REST_APIs/app.js
+++ REST_APIs/app.js
@@ -1,32 +1,25 @@
 const express = require("express");
 const app = express();
+
+//Import Routes
+const userRoutes = require('./routes/users');
 //Import DB Connection
+
 require('./dbConnect');
 
-//Import User Models
-const Users = require('./models/Users');
-
-app.use(express.json());
 
 const port = process.env.PORT || 3000;
 
+app.get('/', (req,res)=>{
+    res.send("This is Home Route");
+});
 
-app.post('/', (req, res) => {
+app.use('/users',userRoutes);
 
-    const user = new Users(req.body);
-
-    user.save((err) => {
-        if (err) {
-            res.send(err);
-            throw err;
-        }
-        res.status(200).json({ "Status": "Successfully Data Stored in DB" });
-    })
-})
-
-
-
-
-app.listen(port, () => {
+app.listen(port, ()=>{
     console.log(`Server Started at ${port}`);
 });
+
+
+
+
--- REST_APIs/dbConnect.js
+++ REST_APIs/dbConnect.js
@@ -1,31 +1,17 @@
 const mongoose = require("mongoose");
-const config = require('./config/default.json');
-
-//Connection to Local DB
-// mongoose.connect("mongodb://127.0.0.1:27017/users", { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
-//     if (err) {
-//         throw err;
-//     }
-//     console.log('Successfully Connected to Mongo DB.');
-// });
-
-//Write it in Promise
-// mongoose.connect("mongodb://localhost/db_name", { useNewUrlParser: true, useUnifiedTopology: true })
-//     .then(() => {
-//         console.log('Successfully Connected to Mongo DB.');
-//     })
-//     .catch((err) => {
-//         console.error('Something Wrong with Mongo DB Server. Please check the connection');
-//     })
-
-//Lets write in async await
+const config = require('./config/index.json')
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(config.MONGO_LOCAL, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex : true });
-        console.log('Successfully Connected to Mongo DB.');
+        await mongoose.connect(config.MONGO_URI,
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+                useCreateIndex: true
+            });
+        console.log("Mongo DB Connected.")
     } catch (err) {
-        console.error('Something Wrong with Mongo DB Server. Please check the connection');
+        console.log(err);
     }
 }
 
