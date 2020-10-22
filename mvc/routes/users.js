fc48a7ba92110dc32173ce3e7b6f1e4ab6744360
ip11 <iprash011@gmail.com>
2020-10-22 08:20:34 +0530

    jade render from DB

--- mvc/routes/users.js
+++ mvc/routes/users.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+//Import the Models
+const User = require('../models/Users');
+
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.render('file', { userName: "prash", age : 25 });
+router.get('/', function (req, res) {
+  res.render('file', { userName: "prash", age: 25 });
 });
+
+router.get('/dashboard/:email', async (req, res) => {
+  try {
+    const email = req.params.email;
+    let userData = await User.findOne({ email: email });
+    userData = userData.toJSON();
+    res.render('dashboard', {
+      firstName: userData.firstName,
+      lastName: userData.lastName
+    });
+  } catch (err) {
+    res.status(500).json({ Error: "Server Error" });
+  }
+
+})
+
 
 module.exports = router;
