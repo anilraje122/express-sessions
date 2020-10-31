0717e296b7f84cb562e9c3668b3946f4b5b82181
ip11 <iprash011@gmail.com>
2020-10-31 07:16:21 +0530

    Implemented Models

---
+++ multi-user-roles-auth/app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+//Import DB
+require('./dbConnect');
+
+
+app.listen(port, () => {
+    console.log(`Server Started at ${port}`);
+});
