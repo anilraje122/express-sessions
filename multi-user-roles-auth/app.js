889d5bbb118b2cfeefa6277e1cbc52e2bffc4ccc
ip11 <iprash011@gmail.com>
2020-10-31 07:29:19 +0530

    Customer Body Data Validation

--- multi-user-roles-auth/app.js
+++ multi-user-roles-auth/app.js
@@ -7,6 +7,9 @@ const port = process.env.PORT || 3000;
 require('./dbConnect');
 
 
+
+
+
 app.listen(port, () => {
     console.log(`Server Started at ${port}`);
 });
--- multi-user-roles-auth/routes/customer/index.js
+++ multi-user-roles-auth/routes/customer/index.js
@@ -1,5 +1,37 @@
 const express = require("express");
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
+
+const bcrypt = require('bcrypt');
+const jwt = require("jsonwebtoken");
+
+
+/*
+Route : /api/customer/register
+To Register New Customer
+Public Route
+*/
+router.post('/register', [
+    body("name", "Please Enter Valid Name").notEmpty().isString(),
+    body("email", "Please Enter Valid EMail").isEmail(),
+    body("role", "Role is Required"), notEmpty(),
+    body("password", "Password is Required").isLength({ min: 6 })
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ "Error": "Server Error" });
+    }
+});
+
+
 
 
 
